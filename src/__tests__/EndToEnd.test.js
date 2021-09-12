eab9c294f0c80e14cb39d3734276b509c4e3b5c5
Jeannie <jntsao@gmail.com>
2021-09-12 16:59:48 -0700

    connected atatus

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,10 +1,11 @@
 import puppeteer from 'puppeteer'
+jest.setTimeout(30000);
 
 describe('show/hide an event details', () => {
     let browser;
     let page;
     beforeAll(async () => {
-        jest.setTimeout(30000);
+
         browser = await puppeteer.launch();
         page = await browser.newPage();
         await page.goto('http://localhost:3000/');
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
+import * as atatus from 'atatus-spa';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -16,8 +17,11 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.unregister();
+atatus.config('575e4186968e48eba198bc2ee2798415').install();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+atatus.notify(new Error('Test Atatus Setup'));
