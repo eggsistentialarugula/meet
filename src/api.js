a1f861d3350390e77f4ea469db44fd95d34c0bb2
Jeannie <jntsao@gmail.com>
2021-09-23 00:29:35 -0700

    fixed error

--- src/api.js
+++ src/api.js
@@ -36,7 +36,7 @@ export const getEvents = async () => {
 
     if (token) {
         removeQuery();
-        const url = 'https://gk2jlemf4g.execute-api.us-west-1.amazonaws.com/dev/api/get-events' + '/' + token;
+        const url = `https://gk2jlemf4g.execute-api.us-west-1.amazonaws.com/dev/api/get-events/${token}`;
         const result = await axios.get(url);
         if (result.data) {
             var locations = extractLocations(result.data.events);
@@ -85,7 +85,8 @@ const removeQuery = () => {
 const getToken = async (code) => {
     const encodeCode = encodeURIComponent(code);
     const { access_token } = await fetch(
-        'https://gk2jlemf4g.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+        `https://gk2jlemf4g.execute-api.us-west-1.amazonaws.com/dev/api/token/${encodeCode}`
+        // 'https://gk2jlemf4g.execute-api.us-west-1.amazonaws.com/dev/api/token' + '/' + encodeCode
     )
         .then((res) => {
             return res.json();
