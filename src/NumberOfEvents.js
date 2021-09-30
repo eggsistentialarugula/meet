2fc5bb5bdd71ce625a3ce53e52882ab8a4103edf
Jeannie <jntsao@gmail.com>
2021-09-29 17:41:29 -0700

    debugt

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -4,7 +4,6 @@ import { ErrorAlert } from './Alert'
 class NumberOfEvents extends React.Component {
     state = {
         numberOfEvents: 32,
-        infoText: '',
     };
 
     handleInputChange = (event) => {
@@ -14,7 +13,7 @@ class NumberOfEvents extends React.Component {
         });
         if (value < 1 || value > 32) {
             this.setState({
-                infoText: 'Please enter/select a number between 1 and 32',
+                infoText: 'Please enter a number between 1 and 32',
             });
         } else {
             this.setState({
