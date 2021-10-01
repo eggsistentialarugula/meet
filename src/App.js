fdfd6cce16fa5fe96f7e66b237544e6205a095af
Jeannie <jntsao@gmail.com>
2021-09-30 23:10:35 -0700

    added getData function

--- src/App.js
+++ src/App.js
@@ -58,6 +58,16 @@ class App extends Component {
     this.updateEvents(currentLocation, eventCount);
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
