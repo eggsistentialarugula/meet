17c2bc2ff4413fe03b517673ea4a5e37ec2d1f9c
Jeannie <jntsao@gmail.com>
2021-09-30 23:27:49 -0700

    edited the scatter chart

--- src/App.js
+++ src/App.js
@@ -82,15 +82,15 @@ class App extends Component {
         <h4>Events in each city</h4>
 
         <ScatterChart
-          width={400}
+          width={800}
           height={400}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
           }}
         >
           <CartesianGrid />
-          <XAxis type="number" dataKey="x" name="stature" unit="cm" />
-          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" allowDecimals={false} />
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill="#8884d8" />
         </ScatterChart>
