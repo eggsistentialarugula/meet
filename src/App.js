6eefd0a4ce61e08ed87917943b2e933877051206
Jeannie <jntsao@gmail.com>
2021-09-30 23:41:32 -0700

    change width of chart

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
         <h4>Events in each city</h4>
 
         <ScatterChart
-          width={800}
+          width={1100}
           height={400}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
