3b9369e55f7725ebc1bdc3fed183237c8e12adac
Jeannie <jntsao@gmail.com>
2021-10-01 03:13:37 -0700

    test

--- src/App.js
+++ src/App.js
@@ -80,9 +80,8 @@ class App extends Component {
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEventCount={(e) => this.updateEventCount(e)} />
         <h4>Events in each city</h4>
-        <EventGenre events={this.state.events} />
         <div className="data-vis-wrapper">
-
+          <EventGenre events={this.state.events} />
           <ResponsiveContainer height={400}>
             <ScatterChart margin={{
               top: 20, right: 20, bottom: 20, left: 20,
