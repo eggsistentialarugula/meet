bb47f823605bacd80d7e4f8bbd655056c1469834
Jeannie <jntsao@gmail.com>
2021-10-01 17:12:59 -0700

    test

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,43 +1,51 @@
-import React, { useEffect, useState } from 'react';
-import {
-    PieChart, Pie, Cell, ResponsiveContainer
-} from 'recharts';
+import React, { PureComponent } from 'react';
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer } from 'recharts';
 
-const EventGenre = ({ events }) => {
-    useEffect(() => { setData(() => getData()); }, [events]);
-    const [data, setData] = useState([]);
+const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+];
 
-    const getData = () => {
-        const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-        const data = genres.map((genre) => {
-            const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-            return { name: genre, value };
-        });
-        return data;
-    };
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 
-    const COLORS = ['#ffcf4d', '#f288e8', '#b391ff', '#879cff', '#66ffe3'];
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
     return (
-        <ResponsiveContainer width="100%" height="100%">
-            <PieChart width={400} height={400}>
-                <Pie
-                    data={data}
-                    cx={200}
-                    cy={200}
-                    labelLine={false}
-                    outerRadius={80}
-                    fill="#8884d8"
-                    dataKey="value"
-                    label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}>
-                    {
-                        data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} name={entry.name} />
-                        ))
-                    }
-                </Pie>
-            </PieChart>
-        </ResponsiveContainer>
-    )
+        <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
+
+export default class Example extends PureComponent {
+    static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
+
+    render() {
+        return (
+            <ResponsiveContainer width="100%" height="100%">
+                <PieChart width={400} height={400}>
+                    <Pie
+                        data={data}
+                        cx="50%"
+                        cy="50%"
+                        labelLine={false}
+                        label={renderCustomizedLabel}
+                        outerRadius={80}
+                        fill="#8884d8"
+                        dataKey="value"
+                    >
+                        {data.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        ))}
+                    </Pie>
+                </PieChart>
+            </ResponsiveContainer>
+        );
+    }
 }
-export default EventGenre;
