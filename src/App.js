4162a9da23d73aca0ae27b374dcb09ace4bc33f1
Jeannie <jntsao@gmail.com>
2021-10-06 01:11:57 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -60,26 +60,47 @@ class App extends Component {
     this.mounted = false;
   }
 
-  updateEvents = (location, eventCount) => {
+  // updateEvents = (location, eventCount) => {
+  //   getEvents().then((events) => {
+  //     let locationEvents = (location === 'all') ?
+  //       events :
+  //       events.filter((event) => event.location === location);
+  //     locationEvents = locationEvents.slice(0, eventCount)
+  //     this.setState({
+  //       events: locationEvents,
+  //       currentLocation: location
+  //     });
+  //   });
+  // }
+
+  // updateEventCount = (eventCount) => {
+  //   this.setState({
+  //     numberOfEvents: eventCount
+  //   });
+  //   const { currentLocation } = this.state;
+  //   this.updateEvents(currentLocation, eventCount);
+  // };
+
+  updateEvents = (location) => {
     getEvents().then((events) => {
-      let locationEvents = (location === 'all') ?
+      const locationEvents = (location === 'all') ?
         events :
         events.filter((event) => event.location === location);
-      locationEvents = locationEvents.slice(0, eventCount)
+      const { numberOfEvents } = this.state;
       this.setState({
-        events: locationEvents,
-        currentLocation: location
+        events: locationEvents.slice(0, numberOfEvents)
       });
     });
   }
 
   updateEventCount = (eventCount) => {
+    const { currentLocation } = this.state;
     this.setState({
       numberOfEvents: eventCount
     });
-    const { currentLocation } = this.state;
     this.updateEvents(currentLocation, eventCount);
-  };
+  }
+
 
   getData = () => {
     const { locations, events } = this.state;
